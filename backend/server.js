df12b030f9d08bc5be442584707a66c69ed402d7
Tamanna <tamannakhanum@hotmail.co.uk>
2025-02-21 14:03:32 +0000

    "Set up server and database connection."

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log('Hello World');
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+
+// Error handling middleware
+
+// Sync database and start server
+async function startServer() {
+  try{
+    await mongoose.connect(process.env.MONGODB_URI)
+    console.log('✅ Database connections established')
+    app.listen(process.env.PORT, () => console.log(`🦻🏽 Server listening on port ${process.env.PORT}`))
+  } catch (error) {
+    console.log('❌ Error establishing connection')
+    console.log(error)
+  }
+}
+
+startServer();
