723099385be56663e2cdc1e8dc6ffe957c5768d1
Tamanna <tamannakhanum@hotmail.co.uk>
2025-03-05 14:47:28 +0000

    "feat(schema): add validation for Company schema and Pharmacist schema."

--- backend/models/company.js
+++ backend/models/company.js
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose';
 import bcrypt from 'mongoose';
 
-//TODO: Configure validation for the company schema
-
 const companySchema = new mongoose.Schema({
   company: { type: String, required: true },
   username: { type: String, required: true, unique: true, maxlength: 30 },
@@ -16,6 +14,43 @@ companySchema.set('toJSON', {
   transform(doc, json){
     delete json.password;
   }
+});
+
+companySchema.virtual('jobsCreated', {
+  ref: 'Job',
+  localField: '_id',
+  foreignField: 'owner',
+});
+
+companySchema
+  .virtual('passwordConfirmation')
+  .set(function(value){
+    this._passwordConfirmation = value;
+  });
+
+companySchema.pre('validate', async function(next){
+  if(this.isModified('password') && this._password !== this.passwordConfirmation){
+    this.invalidate('passwordConfirmation', 'Passwords do not match');
+  };
+
+  const existingUsername = await this.constructor.findOne({ username: this.username })
+  if (existingUsername && existingUsername._id.toString() !== this._id.toString()){
+    this.invalidate('username', 'Username already exists');
+  };
+
+  const existingEmail = await this.constructor.findOne({ email: this.email })
+  if (existingEmail && existingEmail._id.toString() !== this._id.toString()){
+    this.invalidate('email', 'Email already exists');
+  };
+
+  next();
+});
+
+companySchema.pre('save', function(next){
+  if(this.isModified('password')){
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(12));
+  }
+  next();
 });
 
 export default mongoose.model('Company', companySchema);
--- backend/models/pharmacist.js
+++ backend/models/pharmacist.js
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
-//TODO: Configure validation for the pharmacist schema
-
 const pharmacistSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true, maxlength: 30 },
   email: { type: String, required: true, unique: true },
@@ -14,6 +12,37 @@ pharmacistSchema.set('toJSON', {
   transform(doc, json){
     delete json.password;
   }
+});
+
+pharmacistSchema
+  .virtual('passwordConfirmation')
+  .set(function(value){
+    this._passwordConfirmation = value;
+  });
+
+pharmacistSchema.pre('validate', async function(next){
+  if(this.isModified('password') && this._password !== this.passwordConfirmation){
+    this.invalidate('passwordConfirmation', 'Passwords do not match');
+  };
+
+  const existingUsername = await this.constructor.findOne({ username: this.username })
+  if (existingUsername && existingUsername._id.toString() !== this._id.toString()){
+    this.invalidate('username', 'Username already exists');
+  };
+
+  const existingEmail = await this.constructor.findOne({ email: this.email })
+  if (existingEmail && existingEmail._id.toString() !== this._id.toString()){
+    this.invalidate('email', 'Email already exists');
+  };
+
+  next();
+});
+
+pharmacistSchema.pre('save', function(next){
+  if(this.isModified('password')){
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(12));
+  }
+  next();
 });
 
 export default mongoose.model('Pharmacist', pharmacistSchema);
