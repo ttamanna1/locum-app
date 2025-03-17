9b01f693772e1b969b4ff16cf725a4572428bb79
Tamanna <tamannakhanum@hotmail.co.uk>
2025-03-17 12:34:32 +0000

    "feat(jobs): add cancel job functionality with notifications"

--- backend/controllers/jobs.js
+++ backend/controllers/jobs.js
@@ -1,4 +1,5 @@
 import Job from '../models/job.js';
+import Company from '../models/company.js';
 
 // Get all jobs
 // Method: Get
@@ -8,8 +9,8 @@ export const getAllJobs = async (req, res) => {
     const jobs = await Job.find({ bookedBy: null });
     return res.json(jobs);
   } catch (error) {
-    console.error('Error fetching jobs.', error);
-    res.status(500).json({ error: 'Error fetching jobs.' });
+    console.error('Error fetching shifts.', error);
+    res.status(500).json({ error: 'Error fetching shifts.' });
   }
 };
 
@@ -21,12 +22,12 @@ export const getSingleJob = async (req, res) => {
   try {
     const job = await Job.findOne({ _id: jobId, bookedBy: null });
     if (!job) {
-      return res.status(404).json({ error: 'Job not found.' });
+      return res.status(404).json({ error: 'Shift not found.' });
     }
     return res.status(200).json(job);
   } catch (error) {
-    console.error('Error fetching job.', error);
-    res.status(500).json({ error: 'Error fetching job.' });
+    console.error('Error fetching shift.', error);
+    res.status(500).json({ error: 'Error fetching shift.' });
   }
 };
 
@@ -39,8 +40,8 @@ export const getAllCompanyJobs = async (req, res) => {
     const jobs = await Job.find({ owner: companyId, bookedBy: null });
     return res.json(jobs);
   } catch (error) {
-    console.error('Error fetching jobs.', error);
-    res.status(500).json({ error: 'Error fetching jobs.' });
+    console.error('Error fetching shifts.', error);
+    res.status(500).json({ error: 'Error fetching shifts.' });
   }
 };
 
@@ -52,12 +53,12 @@ export const getSingleCompanyJob = async (req, res) => {
   try {
     const job = await Job.findOne({ _id: jobId, owner: companyId, bookedBy: null });
     if (!job) {
-      return res.status(404).json({ error: 'Job not found or does not belong to this company.' });
+      return res.status(404).json({ error: 'Shift not found or does not belong to this company.' });
     }
     return res.status(200).json(job);
   } catch (error) {
-    console.error('Error fetching job.', error);
-    res.status(500).json({ error: 'Error fetching job.' });
+    console.error('Error fetching shift.', error);
+    res.status(500).json({ error: 'Error fetching shift.' });
   }
 };
 
@@ -85,13 +86,13 @@ export const updateJob = async (req, res) => {
       return res.status(401).json({ error: 'Unauthorized' });
     }
     if (!jobToUpdate) {
-      return res.status(404).json({ error: 'Job not found.' });
+      return res.status(404).json({ error: 'shift not found.' });
     }
     const jobToUpdate = await Job.findByIdAndUpdate(jobId, req.body, { new: true });
     return res.json(jobToUpdate);
   } catch (error) {
-    console.error('Error updating job', error);
-    res.status(500).json({ error: 'Error updating job.' });
+    console.error('Error updating shift', error);
+    res.status(500).json({ error: 'Error updating shift.' });
   }
 };
 
@@ -103,7 +104,7 @@ export const deleteJob = async (req, res) => {
     const { jobId } = req.params;
     const jobToDelete = await Job.findByIdAndDelete({ _id: jobId, owner: req.currentCompany._id });
     if (!jobToDelete) {
-      return res.status(400).json({ error: 'Job not found.' });
+      return res.status(400).json({ error: 'shift not found.' });
     }
     return res.sendStatus(204);
   } catch (error) {
@@ -121,8 +122,8 @@ export const getBookedJobsForCompany = async (req, res) => {
     const jobs = await Job.find({ owner: companyId, bookedBy: { $ne: null } }); // $ne: not equal to null
     return res.json(jobs);
   } catch (error) {
-    console.error('Error fetching jobs.', error);
-    res.status(500).json({ error: 'Error fetching jobs.' });
+    console.error('Error fetching shifts.', error);
+    res.status(500).json({ error: 'Error fetching shifts.' });
   }
 };
 
@@ -134,17 +135,17 @@ export const bookJob = async (req, res) => {
     const { jobId } = req.params;
     const job = await Job.findById(jobId);
     if (!job) {
-      return res.status(404).json({ error: 'Job not found.' });
+      return res.status(404).json({ error: 'Shift not found.' });
     }
     if (job.bookedBy) {
-      return res.status(400).json({ error: 'Job already booked.' });
+      return res.status(400).json({ error: 'Shift already booked.' });
     }
     job.bookedBy = req.pharmacist._id;
     await job.save();
     return res.json(job);
   } catch (error) {
-    console.error('Error booking job.', error);
-    res.status(500).json({ error: 'Error booking job.' });
+    console.error('Error booking shift.', error);
+    res.status(500).json({ error: 'Error booking shift.' });
   }
 };
 
@@ -156,8 +157,8 @@ export const getPharmacistJobs = async (req, res) => {
     const jobs = await Job.find({ bookedBy: req.currentPharmacist._id });
     return res.json(jobs);
   } catch (error) {
-    console.error('Error fetching jobs.', error);
-    res.status(500).json({ error: 'Error fetching jobs.' });
+    console.error('Error fetching shifts.', error);
+    res.status(500).json({ error: 'Error fetching shifts.' });
   }
 };
 
@@ -169,11 +170,54 @@ export const getSinglePharmacistJob = async (req, res) => {
   try {
     const job = await Job.findOne({ _id: jobId, bookedBy: req.currentPharmacist._id });
     if (!job) {
-      return res.status(404).json({ error: 'Job not found or booked by another pharmacist.' });
+      return res.status(404).json({ error: 'Shift not found or booked by another pharmacist.' });
     }
     return res.status(200).json(job);
   } catch (error) {
-    console.error('Error fetching job.', error);
-    return res.status(500).json({ error: 'Error fetching job.' });
+    console.error('Error fetching shift.', error);
+    return res.status(500).json({ error: 'Error fetching shift.' });
+  }
+};
+
+// Cancel booked job
+// Method: Post
+// Path: /pharmacist-profile/jobs/:jobId/cancelled
+export const cancelJob = async (req, res) => {
+  const { jobId } = req.params;
+  try {
+    const job = await Job.findOne({ _id: jobId, bookedBy: req.currentPharmacist._id });
+    if (!job) {
+      return res.status(404).json({ error: 'Shift not found or booked by another pharmacist.' });
+    }
+    //Find company to notify cancellation
+    const company = await Company.findById(job.owner);
+    if (!company) {
+      return res.status(404).json({ error: 'Company not found.' });
+    }
+    //Add notification to company
+    const notification = `Shift ${job.pharmacy}, ${jobId} on ${job.date} has been cancelled by ${req.currentPharmacist.name}.`;
+    company.notifications.push(notification);
+    await company.save();
+    // Cancel job
+    job.previousBookedBy = job.bookedBy;
+    job.bookedBy = null;
+    await job.save();
+    return res.status(200).json({ message: 'Shift cancelled successfully.' });
+  } catch (error) {
+    console.error('Error cancelling shift.', error);
+    return res.status(500).json({ error: 'Error cancelling shift' });
+  }
+};
+
+// Get cancelled jobs 
+// Method: Get
+// Path: /pharmacist-profile/jobs/cancelled
+export const getCancelledJobs = async (req, res) => {
+  try {
+    const jobs = await Job.find({ previousBookedBy: req.currentPharmacist._id, bookedBy: null });
+    return res.json(jobs);
+  } catch (error) {
+    console.error('Error fetching shifts.', error);
+    res.status(500).json({ error: 'Error fetching shifts.'});
   }
 };
