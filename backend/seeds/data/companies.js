33c4fd3ca8c79050fd81a435043c4851b6dc858e
Tamanna <tamannakhanum@hotmail.co.uk>
2025-03-27 10:50:31 +0000

    "feat(seed): add seed data for companies"

---
+++ backend/seeds/data/companies.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    company: 'A Ltd',
+    username: 'A Company',
+    email: 'acompany@email.com',
+    password: 'pass',
+    pharmacies: [], 
+    notifications: [],
+  },
+  {
+    company: 'B Ltd',
+    username: 'B Company',
+    email: 'bcompany@email.com',
+    password: 'pass',
+    pharmacies: [], 
+    notifications: [],
+  },
+  {
+    company: 'C Ltd',
+    username: 'C Company',
+    email: 'ccompany@email.com',
+    password: 'pass',
+    pharmacies: [], 
+    notifications: [],
+  },
+  {
+    company: 'D Ltd',
+    username: 'D Company',
+    email: 'dcompany@email.com',
+    password: 'pass',
+    pharmacies: [], 
+    notifications: [],
+  },
+  {
+    company: 'E Ltd',
+    username: 'E Company',
+    email: 'ecompany@email.com',
+    password: 'pass',
+    pharmacies: [], 
+    notifications: [],
+  },
+];
